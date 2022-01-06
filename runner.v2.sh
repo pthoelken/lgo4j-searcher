32577c8b476f324f4748fa2874dee0ab4a9bb684
pthoelken <patrick.thoelken@outlook.com>
2022-01-06 14:41:23 +0100

    wip

--- runner.v2.sh
+++ runner.v2.sh
@@ -90,7 +90,7 @@ function Scanning() {
         *" _OLD_");;                            # HIDE (for the moment)
         *) echo "  - $line" ;;                  # SHOW (the rest)
         esac
-    done < <(sudo find "${find_opt[@]}" > /dev/null 2>&1 | sudo "$objJava" -jar ${objDetector##*/} --stdin | tee -a $strLogUnparsed > /dev/null 2>&1 || true)
+    done < <(sudo find "${find_opt[@]}" | sudo "$objJava" -jar ${objDetector##*/} --stdin | tee -a $strLogUnparsed > /dev/null 2>&1 || true) > /dev/null 2>&1
 }
 
 function ParseLogs() {
