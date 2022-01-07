848e11aa14ff2a4bb292904310c6dd3e05dfe746
Patrick Thoelken <patrick.thoelken@lhind.dlh.de>
2022-01-07 10:47:38 +0100

    wip

--- runner.v2.sh
+++ runner.v2.sh
@@ -71,7 +71,7 @@ function Scanning() {
     warn=()
     while read line; do
 
-        # case "$line" in
+        case "$line" in
         # "-- Problem"*" encrypted "*) ;;         # HIDE
         # "-- Problem"*".zip.ZipException"*) ;;   # HIDE
         # "-- Problem"*".io.EOFException"*) ;;    # HIDE
@@ -84,7 +84,7 @@ function Scanning() {
         # *" _POTENTIALLY_SAFE_"*);;              # HIDE
         # *" _OLD_");;                            # HIDE (for the moment)
         # *) echo "  - $line" ;;                  # SHOW (the rest)
-        # esac
+        esac
 
     done < <(sudo find "${find_opt[@]}" | sudo "$objJava" -jar ${objDetector##*/} --stdin | tee -a $strLogUnparsed || true) > /dev/null 2>&1
 
